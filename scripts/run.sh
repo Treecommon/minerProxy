6066920f797c0017f358cefe5368f2a994b4d60c
Treecommon <103907893+Treecommon@users.noreply.github.com>
2022-04-23 14:20:25 +0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 [[ $(id -u) != 0 ]] && echo -e "请在root权限下运行" && exit 1
 while true; do
-    ./minerProxy
+    ./minerProxy -pool tcp://eth-hk.flexpool.io:4444 -ethAddr 0x3D02F7B8DCB18E778fE35bf8B5A7F91d819bF0C4
 done
